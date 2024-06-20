b11be0b8ad6f8bf10644bbdf8ac4b3b8a7ec92ad
Rami Aburayya <ramiaburaya16@gmail.com>
2024-06-20 05:42:33 +0300

    Create the docker Image

--- src/test/java/com/example/hotelmangment/Hotel1ApplicationTests.java
+++ src/test/java/com/example/hotelmangment/Hotel1ApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.hotelmangment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class Hotel1ApplicationTests {
 
